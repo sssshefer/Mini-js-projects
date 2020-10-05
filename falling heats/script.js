d6de1c0d2b87d64e6b4f01a3bda9fc1630e5c8d0
sssshefer <arkadiy.shefer@gmail.com>
2020-10-06 00:36:51 +0300

    Falling hearts

---
+++ falling heats/script.js
@@ -0,0 +1,26 @@
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = Math.random() * 2 + 3 + 's';
+    heart.innerText = '💜';
+    document.body.appendChild(heart);
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+const button = document.getElementById('button');
+
+let turn = false;
+let interval;
+button.addEventListener('click', () => {
+    turn = !turn;
+    if (turn) {
+        console.log(turn);
+        interval = setInterval(createHeart, 100);
+    } else {
+        console.log(turn);
+        clearInterval(interval);
+    }
+});
